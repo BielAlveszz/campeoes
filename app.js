cc2661e0500b48a3a612092e3e2122ffdfc5358b
Gabriel Gonçalves Alves <167901465+BielAlveszz@users.noreply.github.com>
2024-09-07 04:44:01 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,253 @@
+function pesquisar() {
+    let section = document.getElementById("todas-copas");
+    let suaPesquisa = document.getElementById("sua-pesquisa").value;
+    suaPesquisa = suaPesquisa.toLowerCase()
+    let resultado = "";
+    let titulo = "";
+    let tags = "";
+
+if(suaPesquisa !=="") {
+for (let copa of copas) {
+    titulo = copa.titulo.toLowerCase()
+    tags = copa.tags.toLowerCase()
+    if (titulo.includes(suaPesquisa)){
+    resultado += `
+    <div class="item-resultado">
+                <h2>${copa.titulo}
+                </h2>
+                <p class="descricao-meta">${copa.descricao}</p>
+                 <ul>
+                <h4>${copa.jogadoresConvocados.titulo2}</h4>
+                <ul>
+                    <li>${copa.jogadoresConvocados.titulo3}</li>
+                    <ul>
+                        <li>${copa.jogadoresConvocados.goleiros[0]}</li>
+                        <li>${copa.jogadoresConvocados.goleiros[1]}</li>
+                    </ul>
+                    <li>${copa.jogadoresConvocados.titulo4}</li>
+                    <ul>
+                        <li>${copa.jogadoresConvocados.zagueiros[0]}</li>
+                        <li>${copa.jogadoresConvocados.zagueiros[1]}</li>
+                        <li>${copa.jogadoresConvocados.zagueiros[2]}</li>
+                        <li>${copa.jogadoresConvocados.zagueiros[3]}</li>
+                    </ul>
+                    <li>${copa.jogadoresConvocados.titulo5}</li>
+                    <ul>
+                        <li>${copa.jogadoresConvocados.laterais[0]}</li>
+                        <li>${copa.jogadoresConvocados.laterais[1]}</li>
+                        <li>${copa.jogadoresConvocados.laterais[2]}</li>
+                        <li>${copa.jogadoresConvocados.laterais[3]}</li>
+                    </ul>
+                    <li>${copa.jogadoresConvocados.titulo6}</li>
+                    <ul>
+                        <li>${copa.jogadoresConvocados.meio[0]}</li>
+                        <li>${copa.jogadoresConvocados.meio[1]}</li>
+                        <li>${copa.jogadoresConvocados.meio[2]}</li>
+                        <li>${copa.jogadoresConvocados.meio[3]}</li>
+                    </ul>
+                    <li>${copa.jogadoresConvocados.titulo7}</li>
+                    <ul>
+                     <li>${copa.jogadoresConvocados.ataque[0]}</li>
+                    <li>${copa.jogadoresConvocados.ataque[1]}</li>
+                    <li>${copa.jogadoresConvocados.ataque[2]}</li>
+                    <li>${copa.jogadoresConvocados.ataque[3]}</li>
+                    <li>${copa.jogadoresConvocados.ataque[4]}</li>
+                    <li>${copa.jogadoresConvocados.ataque[5]}</li>
+                    <li>${copa.jogadoresConvocados.ataque[6]}</li>
+                    <li>${copa.jogadoresConvocados.ataque[7]}</li>
+                    </ul>
+                </ul>
+                <p class="descricao-meta">${copa.adversarios}</p>
+                <p class= "descricao-meta">${copa.artilheiroBR}</p>
+                <a href=${copa.link} target="_blank">Mais informações</a>
+</div>
+`
+    }
+}
+
+for (let copa of copaTRI) {
+    titulo = copa.titulo.toLowerCase()
+    tags = copa.tags.toLowerCase()
+    if (copa.titulo.includes(suaPesquisa)){
+    resultado +=
+`
+<div class="item-resultado">
+                <h2>${copa.titulo}
+                </h2>
+                <p class="descricao-meta">${copa.descricao}</p>
+                 <ul>
+                <h4>${copa.jogadoresConvocados.titulo2}</h4>
+                <ul>
+                    <li>${copa.jogadoresConvocados.titulo3}</li>
+                    <ul>
+                        <li>${copa.jogadoresConvocados.goleiros[0]}</li>
+                        <li>${copa.jogadoresConvocados.goleiros[1]}</li>
+                        <li>${copa.jogadoresConvocados.goleiros[2]}</li>
+                    </ul>
+                    <li>${copa.jogadoresConvocados.titulo4}</li>
+                    <ul>
+                        <li>${copa.jogadoresConvocados.zagueiros[0]}</li>
+                        <li>${copa.jogadoresConvocados.zagueiros[1]}</li>
+                        <li>${copa.jogadoresConvocados.zagueiros[2]}</li>
+                        <li>${copa.jogadoresConvocados.zagueiros[3]}</li>
+                    </ul>
+                    <li>${copa.jogadoresConvocados.titulo5}</li>
+                    <ul>
+                        <li>${copa.jogadoresConvocados.laterais[0]}</li>
+                        <li>${copa.jogadoresConvocados.laterais[1]}</li>
+                        <li>${copa.jogadoresConvocados.laterais[2]}</li>
+                        <li>${copa.jogadoresConvocados.laterais[3]}</li>
+                    </ul>
+                    <li>${copa.jogadoresConvocados.titulo6}</li>
+                    <ul>
+                        <li>${copa.jogadoresConvocados.meio[0]}</li>
+                        <li>${copa.jogadoresConvocados.meio[1]}</li>
+                        <li>${copa.jogadoresConvocados.meio[2]}</li>
+                        <li>${copa.jogadoresConvocados.meio[3]}</li>
+                    </ul>
+                    <li>${copa.jogadoresConvocados.titulo7}</li>
+                    <ul>
+                     <li>${copa.jogadoresConvocados.ataque[0]}</li>
+                    <li>${copa.jogadoresConvocados.ataque[1]}</li>
+                    <li>${copa.jogadoresConvocados.ataque[2]}</li>
+                    <li>${copa.jogadoresConvocados.ataque[3]}</li>
+                    <li>${copa.jogadoresConvocados.ataque[4]}</li>
+                    <li>${copa.jogadoresConvocados.ataque[5]}</li>
+                    </ul>
+                </ul>
+                <p class="descricao-meta">${copa.adversarios}</p>
+                <p class= "descricao-meta">${copa.artilheiroBR}</p>
+                <a href=${copa.link} target="_blank">Mais informações</a>
+</div>
+`
+      }
+}
+
+for (let copa of copaTetra) {
+    titulo = copa.titulo.toLowerCase()
+    tags = copa.tags.toLowerCase()
+    if (copa.titulo.includes(suaPesquisa)){
+    resultado +=
+`
+<div class="item-resultado">
+                <h2>${copa.titulo}
+                </h2>
+                <p class="descricao-meta">${copa.descricao}</p>
+                 <ul>
+                <h4>${copa.jogadoresConvocados.titulo2}</h4>
+                <ul>
+                    <li>${copa.jogadoresConvocados.titulo3}</li>
+                    <ul>
+                        <li>${copa.jogadoresConvocados.goleiros[0]}</li>
+                        <li>${copa.jogadoresConvocados.goleiros[1]}</li>
+                        <li>${copa.jogadoresConvocados.goleiros[2]}</li>
+                    </ul>
+                    <li>${copa.jogadoresConvocados.titulo4}</li>
+                    <ul>
+                        <li>${copa.jogadoresConvocados.zagueiros[0]}</li>
+                        <li>${copa.jogadoresConvocados.zagueiros[1]}</li>
+                        <li>${copa.jogadoresConvocados.zagueiros[2]}</li>
+                        <li>${copa.jogadoresConvocados.zagueiros[3]}</li>
+                    </ul>
+                    <li>${copa.jogadoresConvocados.titulo5}</li>
+                    <ul>
+                        <li>${copa.jogadoresConvocados.laterais[0]}</li>
+                        <li>${copa.jogadoresConvocados.laterais[1]}</li>
+                        <li>${copa.jogadoresConvocados.laterais[2]}</li>
+                        <li>${copa.jogadoresConvocados.laterais[3]}</li>
+                    </ul>
+                    <li>${copa.jogadoresConvocados.titulo6}</li>
+                    <ul>
+                        <li>${copa.jogadoresConvocados.meio[0]}</li>
+                        <li>${copa.jogadoresConvocados.meio[1]}</li>
+                        <li>${copa.jogadoresConvocados.meio[2]}</li>
+                        <li>${copa.jogadoresConvocados.meio[3]}</li>
+                        <li>${copa.jogadoresConvocados.meio[4]}</li>
+                        <li>${copa.jogadoresConvocados.meio[5]}</li>
+                    </ul>
+                    <li>${copa.jogadoresConvocados.titulo7}</li>
+                    <ul>
+                     <li>${copa.jogadoresConvocados.ataque[0]}</li>
+                    <li>${copa.jogadoresConvocados.ataque[1]}</li>
+                    <li>${copa.jogadoresConvocados.ataque[2]}</li>
+                    <li>${copa.jogadoresConvocados.ataque[3]}</li>
+                    <li>${copa.jogadoresConvocados.ataque[4]}</li>
+                    </ul>
+                </ul>
+                <p class="descricao-meta">${copa.adversarios}</p>
+                <p class= "descricao-meta">${copa.artilheiroBR}</p>
+                <a href=${copa.link} target="_blank">Mais informações</a>
+</div>
+
+`
+    }
+}
+
+for (let copa of copaPenta) {
+    titulo = copa.titulo.toLowerCase()
+    tags = copa.tags.toLowerCase()
+    if(copa.titulo.includes(suaPesquisa)){
+    resultado +=
+`
+<div class="item-resultado">
+                <h2>${copa.titulo}
+                </h2>
+                <p class="descricao-meta">${copa.descricao}</p>
+                 <ul>
+                <h4>${copa.jogadoresConvocados.titulo2}</h4>
+                <ul>
+                    <li>${copa.jogadoresConvocados.titulo3}</li>
+                    <ul>
+                        <li>${copa.jogadoresConvocados.goleiros[0]}</li>
+                        <li>${copa.jogadoresConvocados.goleiros[1]}</li>
+                        <li>${copa.jogadoresConvocados.goleiros[2]}</li>
+                    </ul>
+                    <li>${copa.jogadoresConvocados.titulo4}</li>
+                    <ul>
+                        <li>${copa.jogadoresConvocados.zagueiros[0]}</li>
+                        <li>${copa.jogadoresConvocados.zagueiros[1]}</li>
+                        <li>${copa.jogadoresConvocados.zagueiros[2]}</li>
+                        <li>${copa.jogadoresConvocados.zagueiros[3]}</li>
+                    </ul>
+                    <li>${copa.jogadoresConvocados.titulo5}</li>
+                    <ul>
+                        <li>${copa.jogadoresConvocados.laterais[0]}</li>
+                        <li>${copa.jogadoresConvocados.laterais[1]}</li>
+                        <li>${copa.jogadoresConvocados.laterais[2]}</li>
+                        <li>${copa.jogadoresConvocados.laterais[3]}</li>
+                    </ul>
+                    <li>${copa.jogadoresConvocados.titulo6}</li>
+                    <ul>
+                        <li>${copa.jogadoresConvocados.meio[0]}</li>
+                        <li>${copa.jogadoresConvocados.meio[1]}</li>
+                        <li>${copa.jogadoresConvocados.meio[2]}</li>
+                        <li>${copa.jogadoresConvocados.meio[3]}</li>
+                        <li>${copa.jogadoresConvocados.meio[4]}</li>
+                        <li>${copa.jogadoresConvocados.meio[5]}</li>
+                        <li>${copa.jogadoresConvocados.meio[6]}</li>
+                        <li>${copa.jogadoresConvocados.meio[7]}</li>
+                        <li>${copa.jogadoresConvocados.meio[8]}</li>
+                    </ul>
+                    <li>${copa.jogadoresConvocados.titulo7}</li>
+                    <ul>
+                     <li>${copa.jogadoresConvocados.ataque[0]}</li>
+                    <li>${copa.jogadoresConvocados.ataque[1]}</li>
+                    <li>${copa.jogadoresConvocados.ataque[2]}</li>
+                    <li>${copa.jogadoresConvocados.ataque[3]}</li>
+                    </ul>
+                </ul>
+                <p class="descricao-meta">${copa.adversarios}</p>
+                <p class= "descricao-meta">${copa.artilheiroBR}</p>
+                <a href=${copa.link} target="_blank">Mais informações</a>
+</div>
+
+`
+    }
+}
+}
+if (resultado !=="") {
+    section.innerHTML = resultado
+}   else {
+    section.innerHTML = "<p>Nenhum resultado encontrado para sua pesquisa.</p>"
+}
+}
---
+++ dados.js
@@ -0,0 +1,127 @@
+let copas = [
+    {
+        titulo : "1958. A Campanha Histórica do Brasil na Copa de 1958",
+        descricao : "A Copa do Mundo de 1958, na Suécia, marcou um antes e um depois na história do futebol brasileiro. Com um time repleto de talentos, a Seleção Canarinho conquistou seu primeiro título mundial e apresentou ao mundo um jovem craque chamado Pelé. A estreia de Pelé: Com apenas 17 anos, Pelé encantou o mundo com sua habilidade e gols decisivos. O futebol artístico: O Brasil jogava um futebol bonito e ofensivo, com muita técnica e improvisação. A geração de ouro: Além de Pelé, a seleção contava com outros grandes jogadores como Garrincha, Didi, Nilton Santos e Gilmar.  A conquista do título: A final contra a Suécia foi marcada pela vitória brasileira por 5 a 2, com dois gols de Pelé.",
+        jogadoresConvocados: {
+            titulo2: ["Jogadores Convocados"],
+            titulo3: ["Goleiros:"],
+            goleiros: ["Castilho", " Gilmar"],
+            titulo4: ["Zagueiros:"],
+            zagueiros: ["Bellini", "Orlando Peçanha", "Zózimo", "Mauro Ramos"],
+            titulo5: ["Laterais:"], 
+            laterais: ["Nilton Santos", "Oreco", "Djalma Santos", "De Sordi"],
+            titulo6: ["Meio de Campo:"],
+            meio: ["Dino Sani", "Zito", "Didi", "Moacir"],
+            titulo7: ["Ataque:"],
+            ataque: ["Mazzola", "Zagallo", "Pelé", "Garrincha", "Joel", "Vavá", "Dida", "Pepe"]
+        },
+            adversarios : "Adversarios do Brasil: Fase de Grupos: Áustria, União Soviética e Inglaterra. Quartas: País de Gales. Semifinal: França. Final: Suécia",
+            artilheiroBR : "Artilheiro do Brasil: Pelé",
+
+            link : "https://www.youtube.com/watch?v=2xFVVKYNr-I",
+            tags: "Pelé pele pelé"
+    },
+    {
+        titulo : "1962. A Seleção Brasileira Bicampeã em 1962",
+        descricao :"A Copa de 1962, realizada no Chile, foi um marco histórico para o futebol brasileiro. Mesmo com a ausência de Pelé, que se lesionou na segunda partida, a seleção conseguiu superar as expectativas e conquistar o bicampeonato mundial. O grande destaque da campanha foi Garrincha. O Anjo de Pernas Tortas assumiu o protagonismo da equipe e, com sua habilidade e dribles desconcertantes, conduziu o Brasil à final. Outros jogadores importantes foram Vavá, Amarildo e Zagallo, que formaram um trio de ataque letal. A final contra a Tchecoslováquia foi marcada pela eficiência brasileira. Os gols de Masopust e Amarildo no primeiro tempo garantiram a vitória e o segundo título mundial para o Brasil. A lesão de Pelé: A perda do Rei do Futebol foi um baque para a equipe, mas serviu como um catalisador para a afirmação de outros jogadores. O protagonismo de Garrincha: O ala direito se tornou o principal jogador da seleção e encantou o mundo com seu futebol mágico. A força do grupo: Mesmo com a ausência de Pelé, o elenco brasileiro demonstrou grande união e determinação. Em resumo, a Copa de 1962 foi uma demonstração de força e talento do futebol brasileiro. A seleção mostrou que era capaz de superar qualquer adversidade e conquistar títulos mesmo sem sua principal estrela.",
+        jogadoresConvocados: {
+            titulo2: ["Jogadores Convocados"],
+            titulo3: ["Goleiros:"],
+            goleiros: ["Castilho", " Gilmar",],
+            titulo4: ["Zagueiros:"],
+            zagueiros: ["Bellini", "Jurandir", "Zózimo", "Mauro Ramos"],
+            titulo5: ["Laterais:"], 
+            laterais: ["Nilton Santos", "Altair", "Djalma Santos", "Jair Marinho"],
+            titulo6: ["Meio de Campo:"],
+            meio: ["Mengálvio", "Zito", "Didi", "Zequinha"],
+            titulo7: ["Ataque:"],
+            ataque: ["Amarildo", "Zagallo", "Pelé", "Garrincha", "Jair", "Vavá", "Coutinho", "Pepe"]
+        },
+            adversarios : "Adversarios do Brasil: Fase de Grupos: México (2x0 Brasil), Tchecoslováquia (0x0) e Espanha (2x1 Brasil). Quartas: Inglaterra (3x1 Brasil). Semifinal: Chile (4x2 Brasil). Final: Tchecoslováquia (3x1 Brasil)",
+            artilheiroBR : "Artilheiros do Brasil: Garrincha e Vavá",
+
+            link : "https://www.youtube.com/watch?v=UdLrhU02RmM",
+            tags : "garrincha lesão bicampeao bicampeão segundo"
+
+    },
+    
+] 
+
+let copaTRI = [
+    {
+        titulo : "1970. A Seleção Brasileira Tricampeã",
+        descricao :"A Copa do Mundo de 1970, realizada no México, é considerada por muitos a maior de todos os tempos e marcou o ápice do futebol brasileiro. A seleção, comandada por Zagallo e capitaneada por Carlos Alberto Torres, apresentou um futebol exuberante e conquistou o tricampeonato mundial de forma invicta. O Rei do Futebol (Pelé), em sua terceira Copa, liderou a equipe com maestria, marcando gols e dando assistências, Tostão, Jairzinho, Rivellino e Gérson, formaram um meio-campo e ataque de altíssimo nível técnico e criatividade, O capitão (Carlos Alberto Torres) comandou a defesa com segurança e marcou um dos gols mais memoráveis da história das Copas, no ângulo de Gordon Banks, na final contra a Itália. O futebol artístico do Brasil: A seleção brasileira encantou o mundo com seu futebol bonito e eficaz. A troca de passes rápidos, os dribles desconcertantes e os gols de placa marcaram a campanha. Jairzinho, por exemplo, marcou em todas as partidas da Copa, um feito inédito. A final histórica: Na final, o Brasil enfrentou a Itália. Em um jogo emocionante, a seleção brasileira venceu por 4 a 1, com gols de Pelé, Gerson, Jairzinho e Carlos Alberto Torres. O gol do capitão, após uma jogada ensaiada, foi considerado um dos mais bonitos da história das Copas. O legado da Copa de 1970: A Copa de 1970 consagrou o Brasil como a maior seleção de futebol de todos os tempos. O time de Zagallo é considerado por muitos o melhor time da história, com um futebol que encantou gerações e se tornou referência para os amantes do esporte.",
+        jogadoresConvocados: {
+            titulo2: ["Jogadores Convocados"],
+            titulo3: ["Goleiros:"],
+            goleiros: ["Félix", " Ado", "Leão"],
+            titulo4: ["Zagueiros:"],
+            zagueiros: ["Brito", "Piazza", "Joel Camargo", "Fontana"],
+            titulo5: ["Laterais:"], 
+            laterais: ["Carlos Alberto", "Zé Maria", "Marco Antônio", "Everaldo"],
+            titulo6: ["Meio de Campo:"],
+            meio: ["Clodoaldo", "Gérson", "Rivelino", "Paulo Cézar"],
+            titulo7: ["Ataque:"],
+            ataque: ["Jairzinho", "Tostão", "Pelé", "Roberto", "Edu", "Dario"]
+        },
+            adversarios : "Adversarios do Brasil: Fase de Grupos: Tchecoslováquia (4x1 Brasi), Inglaterra (1x0 Brasil) e Romênia (3x2 Brasil). Quartas: Peru (4x2 Brasil). Semifinal: Chile (3x1 Brasil). Final: Itália (4x1 Brasil)",
+            artilheiroBR : "Artilheiro do Brasil: Jairzinho",
+
+            link : "https://www.youtube.com/watch?v=nQ5nflvo5fA",
+            tags : "tri Tri esquadrão melhor rivelino jairzinho"
+    }
+]
+
+let copaTetra = [
+    {
+        titulo : "1994. É Treta!",
+        descricao :"A Copa do Mundo de 1994, realizada nos Estados Unidos, foi marcada pela conquista do quarto título mundial pela Seleção Brasileira. Após 24 anos de jejum, o tetracampeonato veio em um contexto bastante diferente dos títulos anteriores, com um futebol mais pragmático e eficiente. O baixinho (Romário) foi o grande destaque da equipe, com seus gols e liderança dentro de campo. Já Bebeto formou uma dupla de ataque memorável com Romário e celebrou seus gols com a famosa comemoração do Embala neném. Taffarel foi fundamental nas disputas de pênaltis, especialmente na final contra a Itália. Dunga (o capitão) liderou a equipe com muita garra e determinação. O caminho até o título: A campanha brasileira foi marcada pela solidez defensiva e pela eficiência no ataque. A equipe sofreu apenas três gols em toda a competição e mostrou um grande poder de reação em momentos difíceis. A final contra a Itália: A decisão da Copa foi para os pênaltis, após um empate sem gols no tempo normal e na prorrogação. Taffarel defendeu duas cobranças italianas, enquanto todos os brasileiros converteram, garantindo o tetracampeonato para o Brasil. O legado da Copa de 1994: O tetracampeonato de 1994 marcou o fim de um jejum e consolidou o Brasil como a maior seleção de futebol de todos os tempos. A conquista também mostrou que o sucesso pode ser alcançado com diferentes estilos de jogo, sem abrir mão da eficiência e da determinação. ",
+        jogadoresConvocados: {
+            titulo2: ["Jogadores Convocados"],
+            titulo3: ["Goleiros:"],
+            goleiros: ["Taffarel", " Zetti", "Gilmar"],
+            titulo4: ["Zagueiros:"],
+            zagueiros: ["Ricardo Rocha", "Ronaldão", "Aldair", "Márcio Santos"],
+            titulo5: ["Laterais:"], 
+            laterais: ["Branco", "Leonardo", "Jorginho", "Cafu"],
+            titulo6: ["Meio de Campo:"],
+            meio: ["Mazinho", "Mauro Silva", "Dunga", "Zinho", "Raí", "Paulo Sérgio"],
+            titulo7: ["Ataque:"],
+            ataque: ["Bebeto", "Romário", "Muller", "Ronaldo", "Viola"]
+        },
+            adversarios : "Adversarios do Brasil: Fase de Grupos: Rússia (2x0 Brasi), Camarões (3x0 Brasil) e Suécia (1x1). Oitavas: Estados Unidos (1x0 Brasil). Quartas: Holanda (3x2 Brasil). Semifinal: Suécia (1x0 Brasil). Final: Itália (0x0 PN(3x2) Brasil)",
+            artilheiroBR : "Artilheiro do Brasil: Romário",
+
+            link : "https://www.youtube.com/watch?v=eddbp01QCgI",
+            tags : "treta bebeto romario romário dunda "
+    }
+
+
+]
+
+let copaPenta = [
+    {
+        titulo : "2002. O Pentacampeonato",
+        descricao :"A Copa do Mundo de 2002, realizada na Coreia do Sul e no Japão, marcou a conquista do quinto título mundial pela Seleção Brasileira. Após o traumático fracasso na Copa de 1998, a equipe comandada por Luiz Felipe Scolari se recuperou e apresentou um futebol eficiente e pragmático. O caminho até o penta: A campanha brasileira foi marcada pela eficiência e pela capacidade de se adaptar a diferentes adversários. A seleção venceu todos os jogos da competição, demonstrando uma grande força de vontade e determinação. A final contra a Alemanha: Na final, o Brasil enfrentou a Alemanha em um jogo emocionante. Ronaldo marcou duas vezes e garantiu a vitória brasileira por 2 a 0, dando ao país o seu quinto título mundial. O Fenômeno: Após superar problemas de peso e lesões, Ronaldo voltou a ser o principal jogador da seleção e liderou a equipe rumo ao título. Um time equilibrado: Experiência e juventude: A seleção brasileira combinou a experiência de jogadores como Cafu, Roberto Carlos e Rivaldo com a juventude de Ronaldinho Gaúcho e Kaká. Solidez defensiva: A equipe apresentou uma defesa sólida, com Marcos, Lúcio e Roque Júnior, garantindo a segurança necessária para conquistar o título. O legado da Copa de 2002: A Copa de 2002 marcou a volta do Brasil ao topo do futebol mundial. A conquista do pentacampeonato consolidou a Seleção Brasileira como a maior de todos os tempos e reavivou a paixão dos brasileiros pelo futebol.",
+        jogadoresConvocados: {
+            titulo2: ["Jogadores Convocados"],
+            titulo3: ["Goleiros:"],
+            goleiros: ["Marcos", "Dida", "Rogério Ceni"],
+            titulo4: ["Zagueiros:"],
+            zagueiros: ["Lúcio", "Roque Júnior", "Anderson Polga"],
+            titulo5: ["Laterais:"], 
+            laterais: ["Cafu", "Belletti", "Roberto Carlos", "Júnior"],
+            titulo6: ["Meio de Campo:"],
+            meio: ["Edmílson", "Gilberto Silva", "Kléberson", "Vampeta", "Ricardinho", "Ronaldinho Gaúcho", "Denílson", "Juninho Paulista", "Kaká"],
+            titulo7: ["Ataque:"],
+            ataque: ["Rivaldo", "Edílson", "Luizão", "Ronaldo"]
+        },
+            adversarios : "Adversarios do Brasil: Fase de Grupos: Turquia(2x1 Brasi), China (4x0 Brasil) e Costa Rica (5x2 Brasil). Oitavas: Bélgica (2x0 Brasil). Quartas: Inglaterra (2x1 Brasil). Semifinal: Turquia (1x0 Brasil). Final: Itália (2x0 Brasil)",
+            artilheiroBR : "Artilheiro do Brasil: Ronaldo",
+
+            link : "https://www.youtube.com/watch?v=FSBJCD9-EHE",
+            tags : "penta ronaldo fenomeno fenômeno ronaldinho rivaldo"
+    }
+
+
+]
